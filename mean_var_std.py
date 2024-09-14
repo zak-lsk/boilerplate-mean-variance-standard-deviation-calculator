734ef3d41039a776c00a025225935a59ff97fa73
zak-lsk Lskr <zakaria.lassakeur@edu.uah.es>
2024-09-14 18:02:11 +0000

    Challenge completed

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
+    #reshape the list to matrix 3x3
+    try: 
+        matrix = np.array(list).reshape(3, 3)
 
-
+        calculations = {
+            'mean': [matrix.mean(axis = 0).tolist(), matrix.mean(axis = 1).tolist(), matrix.mean()], 
+            'variance': [matrix.var(axis = 0).tolist(), matrix.var(axis = 1).tolist(), matrix.var()], 
+            'standard deviation': [matrix.std(axis = 0).tolist(), matrix.std(axis = 1).tolist(), matrix.std()], 
+            'max': [matrix.max(axis = 0).tolist(), matrix.max(axis = 1).tolist(), matrix.max()], 
+            'min': [matrix.min(axis = 0).tolist(), matrix.min(axis =  1).tolist(), matrix.min()], 
+            'sum': [matrix.sum(axis = 0).tolist(), matrix.sum(axis =  1).tolist(), matrix.sum()]
+        }
+    except :
+        raise ValueError("List must contain nine numbers.") 
 
 
     return calculations
